232612562e8362f5a552c3a53f45e36269619b22
Nathan Baldwin <nathansbaldwin@gmail.com>
2016-03-12 12:52:10 -0600

    installed and connected mongo

---
+++ Routes/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+
+const register = require('./register')
+const login = require('./login')
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,25 @@
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 const bodyParser = require('body-parser')
+const routes = require('./routes')
 
+//envirnoment variables:
 const PORT = process.env.PORT || 3000
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017
+const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME || 'clocker2'
+
+//use jade to render templates:
+app.set('view engine', 'jade');
 
 //middleware:
 app.use(bodyParser.urlencoded({
   extended:false
 }))
+
+
 
 //create paths to public directory:
 app.use(express.static('public'));
@@ -20,7 +30,14 @@ app.get('/', (req, res) => {
   res.sendfile('./public/')
 })
 
-//tell server to listen on specified port:
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+//connect to specified mongodb before starting server:
+mongoose.connect(`mongodb://localhost:${MONGODB_PORT}/${MONGODB_DB_NAME}`)
+
+mongoose.connection.on('open', () => {
+  console.log(`Mongo db: ${MONGODB_DB_NAME} open on port ${MONGODB_PORT}`);
+
+  app.listen(PORT, () => {
+    console.log(`Node.js server started. Listening on port ${PORT}`);
+  });
 })
+
