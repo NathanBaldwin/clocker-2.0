5521ddc77607e3a39643d64d7e5bb99dbb11ee80
Nathan <nathansbaldwin@gmail.com>
2016-03-29 18:48:44 -0400

    saving changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME || 'clocker2_1'
 //MIDDLEWARE:
 app.use(function (req, res, next) {
   // IP's we want to allow access:
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8101')
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100')
   // 'http://127.0.0.1:8080'
   // Request methods to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
